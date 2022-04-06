793d3372410496c75a8384fe863447aebc179693
Guilherme Dakuzaku <gdakuzak@gmail.com>
2022-04-06 17:33:33 -0300

    :zap:

--- README.md
+++ README.md
@@ -2,3 +2,35 @@
 
 ## ATENCAO: PACOTE TEM DESENVOLVIMENTO. NÃO UTILIZAR!
 
+<hr />
+
+## Dependencia
+
+- Laravel
+
+<hr />
+
+## Testes:
+- Laravel
+    - 8.x ❌
+    - 7.x ❌
+    - 6.x ❌
+    - 5.x ❌
+
+<hr />
+
+## Como usar:
+
+- instale o pacode na sua aplicação:
+``` shell
+    composer require rocketti/dependecy-pattern
+```
+- add no `.env` da aplicação a chave com o nome da pasta onde vai ficar os arquivos de dependencia.
+```shell
+DEPENDENCY_FOLDER="Rocketti"
+```
+- Execute o comando abaixo para criar os arquivos:
+```shell 
+dp:create {class_name} {table_name} {--check}
+```
+Opção `--check` é para verificar se existe as pastas para colocar os arquivos dentro.
--- src/Console/CreateDependencyPatternFiles.php
+++ src/Console/CreateDependencyPatternFiles.php
@@ -4,16 +4,307 @@ namespace Rocketti\DependecyPattern\Console;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Storage;
 
 class CreateDependencyPatternFiles extends Command
 {
     // protected $hidden = true;
-    protected $signature = 'dp:create';
+    protected $signature = 'dp:create {class_name} {table_name} {--check}';
 
     protected $description = 'Create depedency pattern files';
 
     public function handle()
     {
-        $this->info('teste dependencia');
+        $name = $this->argument('name');
+        $class_name = $this->argument('class_name');
+        $table_name = $this->argument('table_name');
+        $class = "App\\".env('DEPENDENCY_FOLDER');
+
+        if($this->option('check'))
+        {
+            $this->line('Checking if folder exists...');
+            $teste = $this->folderExists($class);
+            if($teste === 1) { return 1; }
+            $this->line('Checking if folder exists... done.');
+        }
+
+        $this->line('Creating model...');
+        $this->createModel($class_name,$class,$table_name);
+        $this->line('Creating model... done.');
+
+        $this->line('Creating repository...');
+        $this->createRepository($class_name,$class);
+        $this->line('Creating repository... done.');
+
+        $this->line('Creating service...');
+        $this->createService($class_name,$class);
+        $this->line('Creating service... done.');
+
+    }
+
+    private function folderExists($class)
+    {
+        if(env('DEPENDENCY_FOLDER') == '' || env('DEPENDENCY_FOLDER') == null)
+        {
+            $this->error("                                                                                                                        ");
+            $this->error('  Please, check the env variable -> DEPENDENCY_FOLDER                                                                   ');
+            $this->error("                                                                                                                        ");
+            return 1;
+        }
+
+        $folderName = 'app/'.env('DEPENDENCY_FOLDER');
+        $folders = [
+            'Contracts','Services','Repositories','Models'
+        ];
+        if(!File::exists($folderName))
+        {
+            File::makeDirectory($folderName);
+            foreach($folders as $folder)
+            {
+                File::makeDirectory($folderName."/".$folder);
+            }
+        } elseif (File::exists($folderName)) {
+            foreach($folders as $folder)
+            {
+                if(!File::exists($folderName."/".$folder))
+                {
+                    File::makeDirectory($folderName."/".$folder);
+                }
+            }
+        }
+
+        // check contract file:
+
+        if(!File::exists($folderName."/Contracts/ServiceContract.php"))
+        {
+            $content = '<?php
+
+namespace '.$class.'\Contracts;
+            
+interface ServiceContract
+{
+    /**
+    * @return mixed
+     */
+    public function renderList();
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function renderEdit($id);
+
+    /**
+     * @param $id
+     * @param $data
+     * @return mixed
+     */
+    public function buildUpdate($id, $data);
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public function buildInsert($data);
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function buildDelete($id);
+}';
+            File::put($folderName."/Contracts/ServiceContract.php",$content);
+        }
+        
+    }
+
+    private function createModel($class_name,$class,$table_name)
+    {
+        $folderName = 'app/'.env('DEPENDENCY_FOLDER').'/';
+        if(!File::exists($folderName."Models/".ucfirst($class_name).".php"))
+        {
+            $columns = Schema::getColumnListing($table_name);
+            $columns = "'".implode("','", $columns)."'";
+            $content = '<?php
+
+namespace '.$class.'\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class '.ucfirst($class_name).' extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = "'.$table_name.'";
+
+    protected $fillable = [
+        '.$columns.'
+    ];
+}';
+        File::put($folderName."Models/".ucfirst($class_name).".php",$content);
+        }
+    }
+
+    private function createRepository($class_name,$class)
+    {
+        $folderName = 'app/'.env('DEPENDENCY_FOLDER').'/';
+        if(!File::exists($folderName."Repositories/".ucfirst($class_name)."Repository.php"))
+        {
+
+        $content = '<?php
+
+namespace '.$class.'\Repositories;
+
+use '.$class.'\Models\\'.ucfirst($class_name).';
+
+class '.ucfirst($class_name).'Repository
+{
+    /**
+     * @var Role
+     */
+    private $model;
+
+    /**
+     * '.ucfirst($class_name).' Repository constructor.
+     * @param '.ucfirst($class_name).' $example
+     */
+    public function __construct('.$class_name.' $'.lcfirst($class_name).')
+    {
+        $this->model = $'.$class_name.';
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAll()
+    {
+        return $this->model->get();
+    }
+    
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function getById($id)
+    {
+        return $this->model->find($id);
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public function create($data)
+    {
+        return $this->model->create($data);
+    }
+
+    /**
+     * @param $id
+     * @param $data
+     * @return mixed
+     */
+    public function update($id, $data)
+    {
+        return $this->model->find($id)->fill($data)->save();
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function delete($id)
+    {
+        return $this->model->find($id)->delete();
+    }
+}';
+            File::put($folderName."Repositories/".ucfirst($class_name)."Repository.php",$content);
+        }  
+    }
+
+    private function createService($class_name,$class)
+    {
+        $folderName = 'app/'.env('DEPENDENCY_FOLDER').'/';
+        if(!File::exists($folderName."Services/".ucfirst($class_name)."Service.php"))
+        {
+            $content = '<?php
+
+namespace '.$class.'\Services;
+
+use '.$class.'\Contracts\ServiceContract;
+use '.$class.'\Repositories\\'.$class_name.'Repository;
+
+class '.$class_name.'Service implements ServiceContract
+{
+     /**
+     * @var '.$class_name.'Repository
+     */
+    private $repository;
+
+     /**
+     * @var '.$class_name.'Repository
+     */
+    private $'.lcfirst($class_name).'Repository;
+
+    /**
+     * '.$class_name.' Service constructor.
+     * @param '.$class_name.'Repository $'.lcfirst($class_name).'Repository
+     */
+    public function __construct('.$class_name.'Repository $'.lcfirst($class_name).'Repository)
+    {
+        $this->repository = $'.lcfirst($class_name).'Repository;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function renderList()
+    {
+        return $this->repository->getAll();
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function renderEdit($id)
+    {
+        return $this->repository->getById($id);
+    }
+
+    /**
+     * @param $id
+     * @param $data
+     * @return mixed
+     */
+    public function buildUpdate($id, $data)
+    {
+        return $this->repository->update($id, $data);
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public function buildInsert($data)
+    {
+        return $this->repository->create($data);
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function buildDelete($id)
+    {
+        return $this->repository->delete($id);
+    }
+}';
+            File::put($folderName."Services/".ucfirst($class_name)."Service.php",$content);
+        }
     }
 }
