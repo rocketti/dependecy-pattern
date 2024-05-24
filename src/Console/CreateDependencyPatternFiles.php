f5a56c27d6020491f4b46b24c3c921ec79d417b6
Guilherme Dakuzaku <gdakuzak@gmail.com>
2024-05-24 17:55:19 -0300

    change update to recreate

--- src/Console/CreateDependencyPatternFiles.php
+++ src/Console/CreateDependencyPatternFiles.php
@@ -9,64 +9,66 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateDependencyPatternFiles extends Command
 {
+    const TYPE_UPDATE = 'update';
+    const TYPE_RECREATE = 'recreate';
+
     // protected $hidden = true;
-    protected $signature = 'dp:file {class_name} {table_name} {--check} {--update}';
+    protected $signature = 'dp:file {class_name} {table_name} {--check} {--recreate}';
 
     protected $description = 'Create and update depedency pattern files';
 
     public function handle()
     {
         $validator = $this->validateDependencies();
-        if($validator == 1) {
+        if ($validator == 1) {
             return 1;
         }
 
         $operation = 'create';
 
-        if($this->option('update')){
-            $operation = 'update';
+        if ($this->option('recreate')) {
+            $operation = self::TYPE_RECREATE;
         }
 
         $class_name = $this->argument('class_name');
         $table_name = $this->argument('table_name');
-        $class = "App\\".env('DEPENDENCY_FOLDER');
-        $folderName = (env('APP_ENV') == 'testing')?'./tests/app/'.env('DEPENDENCY_FOLDER')."/":'app/'.env('DEPENDENCY_FOLDER')."/";
+        $class = "App\\" . env('DEPENDENCY_FOLDER');
+        $folderName = (env('APP_ENV') == 'testing') ? './tests/app/' . env('DEPENDENCY_FOLDER') . "/" : 'app/' . env('DEPENDENCY_FOLDER') . "/";
 
-        if($this->option('check'))
-        {
+        if ($this->option('check')) {
             $this->line('Checking if folder exists...');
-            $this->folderExists($folderName,$class);
+            $this->folderExists($folderName, $class);
             $this->line('Checking if folder exists... done.');
         }
 
         $this->line('Creating model...');
-        $this->modelFile($folderName,$class_name,$class,$table_name,$operation);
+        $this->modelFile($folderName, $class_name, $class, $table_name, $operation);
 
         $this->line('Creating repository...');
-        $this->RepositoryFile($folderName,$class_name,$class,$operation);
+        $this->RepositoryFile($folderName, $class_name, $class, $operation);
 
         $this->line('Creating service...');
-        $this->serviceFile($folderName,$class_name,$class,$operation);
+        $this->serviceFile($folderName, $class_name, $class, $operation);
         return 0;
     }
 
     private function validateDependencies()
     {
-        if(env('DEPENDENCY_FOLDER') == '' || env('DEPENDENCY_FOLDER') == null) {
+        if (env('DEPENDENCY_FOLDER') == '' || env('DEPENDENCY_FOLDER') == null) {
             $this->error("                                                                                                                        ");
             $this->error('  Please, check the env variable -> DEPENDENCY_FOLDER                                                                   ');
             $this->error("                                                                                                                        ");
             return 1;
         }
 
-        if($this->argument('class_name') == null  || $this->argument('class_name') == '') {
+        if ($this->argument('class_name') == null  || $this->argument('class_name') == '') {
             $this->error("                                                                                                                        ");
             $this->error('  Please, check argument class_name                                                                                     ');
             $this->error("                                                                                                                        ");
             return 1;
         }
-        
-        if($this->argument('table_name') == null ||$this->argument('table_name') == '') {
+
+        if ($this->argument('table_name') == null || $this->argument('table_name') == '') {
             $this->error("                                                                                                                        ");
             $this->error('  Please, check argument table_name                                                                                     ');
             $this->error("                                                                                                                        ");
@@ -75,7 +77,7 @@ class CreateDependencyPatternFiles extends Command
 
         DB::connection()->getPdo();
 
-        if(!Schema::hasTable($this->argument('table_name'))){
+        if (!Schema::hasTable($this->argument('table_name'))) {
             $this->error("                                                                                                                        ");
             $this->error('  Please, check if table was created.                                                                                   ');
             $this->error("                                                                                                                        ");
@@ -85,35 +87,31 @@ class CreateDependencyPatternFiles extends Command
         return 0;
     }
 
-    private function folderExists($folderName,$class)
+    private function folderExists($folderName, $class)
     {
         $folders = [
-            'Contracts','Services','Repositories','Models'
+            'Contracts', 'Services', 'Repositories', 'Models'
         ];
-        if(!File::exists($folderName))
-        {
+
+        if (!File::exists($folderName)) {
             File::makeDirectory($folderName);
-            foreach($folders as $folder)
-            {
-                File::makeDirectory($folderName."/".$folder);
+            foreach ($folders as $folder) {
+                File::makeDirectory($folderName . "/" . $folder);
             }
         } elseif (File::exists($folderName)) {
-            foreach($folders as $folder)
-            {
-                if(!File::exists($folderName."/".$folder))
-                {
-                    File::makeDirectory($folderName."/".$folder);
+            foreach ($folders as $folder) {
+                if (!File::exists($folderName . "/" . $folder)) {
+                    File::makeDirectory($folderName . "/" . $folder);
                 }
             }
         }
 
         // check contract file:
 
-        if(!File::exists($folderName."/Contracts/ServiceContract.php"))
-        {
+        if (!File::exists($folderName . "/Contracts/ServiceContract.php")) {
             $content = '<?php
 
-namespace '.$class.'\Contracts;
+namespace ' . $class . '\Contracts;
             
 interface ServiceContract
 {
@@ -147,89 +145,85 @@ interface ServiceContract
      */
     public function buildDelete($id);
 }';
-            File::put($folderName."/Contracts/ServiceContract.php",$content);
+            File::put($folderName . "/Contracts/ServiceContract.php", $content);
         }
-        
     }
 
-    private function modelFile($folderName,$class_name,$class,$table_name,$operation)
+    private function modelFile($folderName, $class_name, $class, $table_name, $operation)
     {
-        if(!File::exists($folderName."Models/".ucfirst($class_name).".php") || $operation == 'update')
-        {
+        if (!File::exists($folderName . "Models/" . ucfirst($class_name) . ".php") || $operation == self::TYPE_RECREATE) {
             $columns = Schema::getColumnListing($table_name);
             $castsFinal = [];
 
             foreach ($columns as $key => $column) {
 
-                if(in_array($column,['id','created_at','updated_at'])) {
+                if (in_array($column, ['id', 'created_at', 'updated_at'])) {
                     unset($columns[$key]);
                     continue;
                 }
 
-                $type = Schema::getColumnType($table_name,$column);
-                if(in_array($type,['date','timestamp'])) {
-                    if($type == 'date') {
+                $type = Schema::getColumnType($table_name, $column);
+                if (in_array($type, ['date', 'timestamp'])) {
+                    if ($type == 'date') {
                         $castsFinal[] = "'$column' => 'date'";
                     }
-                    if($type == 'timestamp') {
+                    if ($type == 'timestamp') {
                         $castsFinal[] = "'$column' => 'datetime'";
                     }
                 }
             }
 
-            $columnsFinal = "'".implode("','", $columns)."'";
+            $columnsFinal = "'" . implode("','", $columns) . "'";
             $casts = implode("','", $castsFinal);
 
             $content = '<?php
 
-namespace '.$class.'\Models;
+namespace ' . $class . '\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class '.ucfirst($class_name).' extends Model
+class ' . ucfirst($class_name) . ' extends Model
 {
     /**
      * The table associated with the model.
      *
      * @var string
      */
-    protected $table = "'.$table_name.'";
+    protected $table = "' . $table_name . '";
 
     protected $casts = [
-        '.$casts.'
+        ' . $casts . '
     ];
 
     protected $fillable = [
-        '.$columnsFinal.'
+        ' . $columnsFinal . '
     ];
 }';
-            File::put($folderName."Models/".ucfirst($class_name).".php",$content);
+            File::put($folderName . "Models/" . ucfirst($class_name) . ".php", $content);
 
-            if($operation == 'create') {
+            if ($operation == 'create') {
                 $this->info('Creating model... done.');
             }
 
-            if($operation == 'update') {
-                $this->info('Updating model... done.');
+            if ($operation == 'recreate') {
+                $this->info('Recreating model... done.');
             }
-
         } else {
             $this->warn('Model exists... skipping.');
         }
     }
 
-    private function repositoryFile($folderName,$class_name,$class,$operation)
+    private function repositoryFile($folderName, $class_name, $class, $operation)
     {
-        if(!File::exists($folderName."Repositories/".ucfirst($class_name)."Repository.php") || $operation == 'update')
-        {
+        if (!File::exists($folderName . "Repositories/" . ucfirst($class_name) . "Repository.php") || $operation == self::TYPE_RECREATE) {
 
-        $content = '<?php
+            $content = '<?php
 
-namespace '.$class.'\Repositories;
+namespace ' . $class . '\Repositories;
 
-use '.$class.'\Models\\'.ucfirst($class_name).';
+use ' . $class . '\Models\\' . ucfirst($class_name) . ';
 
-class '.ucfirst($class_name).'Repository
+class ' . ucfirst($class_name) . 'Repository
 {
     /**
      * @var Role
@@ -237,12 +231,12 @@ class '.ucfirst($class_name).'Repository
     private $model;
 
     /**
-     * '.ucfirst($class_name).' Repository constructor.
-     * @param '.ucfirst($class_name).' $'.lcfirst($class_name).'
+     * ' . ucfirst($class_name) . ' Repository constructor.
+     * @param ' . ucfirst($class_name) . ' $' . lcfirst($class_name) . '
      */
-    public function __construct('.$class_name.' $'.lcfirst($class_name).')
+    public function __construct(' . $class_name . ' $' . lcfirst($class_name) . ')
     {
-        $this->model = $'.lcfirst($class_name).';
+        $this->model = $' . lcfirst($class_name) . ';
     }
 
     /**
@@ -290,50 +284,49 @@ class '.ucfirst($class_name).'Repository
         return $this->model->find($id)->delete();
     }
 }';
-            File::put($folderName."Repositories/".ucfirst($class_name)."Repository.php",$content);
-            
-            if($operation == 'create') {
+            File::put($folderName . "Repositories/" . ucfirst($class_name) . "Repository.php", $content);
+
+            if ($operation == 'create') {
                 $this->info('Creating repository... done.');
             }
-    
-            if($operation == 'update') {
-                $this->info('Updating repository... done.');
+
+            if ($operation == self::TYPE_RECREATE) {
+                $this->info('Recreating repository... done.');
             }
         } else {
             $this->warn('Repository exists... skipping.');
         }
     }
 
-    private function serviceFile($folderName,$class_name,$class,$operation)
+    private function serviceFile($folderName, $class_name, $class, $operation)
     {
-        if(!File::exists($folderName."Services/".ucfirst($class_name)."Service.php") || $operation == 'update')
-        {
+        if (!File::exists($folderName . "Services/" . ucfirst($class_name) . "Service.php") || $operation == self::TYPE_RECREATE) {
             $content = '<?php
 
-namespace '.$class.'\Services;
+namespace ' . $class . '\Services;
 
-use '.$class.'\Contracts\ServiceContract;
-use '.$class.'\Repositories\\'.$class_name.'Repository;
+use ' . $class . '\Contracts\ServiceContract;
+use ' . $class . '\Repositories\\' . $class_name . 'Repository;
 
-class '.$class_name.'Service implements ServiceContract
+class ' . $class_name . 'Service implements ServiceContract
 {
      /**
-     * @var '.$class_name.'Repository
+     * @var ' . $class_name . 'Repository
      */
     private $repository;
 
      /**
-     * @var '.$class_name.'Repository
+     * @var ' . $class_name . 'Repository
      */
-    private $'.lcfirst($class_name).'Repository;
+    private $' . lcfirst($class_name) . 'Repository;
 
     /**
-     * '.$class_name.' Service constructor.
-     * @param '.$class_name.'Repository $'.lcfirst($class_name).'Repository
+     * ' . $class_name . ' Service constructor.
+     * @param ' . $class_name . 'Repository $' . lcfirst($class_name) . 'Repository
      */
-    public function __construct('.$class_name.'Repository $'.lcfirst($class_name).'Repository)
+    public function __construct(' . $class_name . 'Repository $' . lcfirst($class_name) . 'Repository)
     {
-        $this->repository = $'.lcfirst($class_name).'Repository;
+        $this->repository = $' . lcfirst($class_name) . 'Repository;
     }
 
     /**
@@ -381,15 +374,14 @@ class '.$class_name.'Service implements ServiceContract
         return $this->repository->delete($id);
     }
 }';
-            File::put($folderName."Services/".ucfirst($class_name)."Service.php",$content);
-            if($operation == 'create') {
+            File::put($folderName . "Services/" . ucfirst($class_name) . "Service.php", $content);
+            if ($operation == 'create') {
                 $this->info('Creating service... done.');
             }
 
-            if($operation == 'update') {
-                $this->info('Updating service... done.');
+            if ($operation == self::TYPE_RECREATE) {
+                $this->info('Recreating service... done.');
             }
-
         } else {
             $this->warn('Service exists... skipping.');
         }
